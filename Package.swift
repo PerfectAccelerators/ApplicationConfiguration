a295b47273b2450d51328e3afb2dec7fd3f31448
conqueror <f.nayebi@gmail.com>
2018-05-15 17:26:17 -0400

    Dependency updates

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,15 +12,15 @@ let package = Package(
             targets: ["ApplicationConfiguration"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-CRUD.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ApplicationConfiguration",
-            dependencies: []),
+            dependencies: ["PerfectHTTPServer", "PerfectCRUD"]),
         .testTarget(
             name: "ApplicationConfigurationTests",
             dependencies: ["ApplicationConfiguration"]),
