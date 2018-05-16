f9766a3a53f0aeca2184a8753a1129855a09216c
conqueror <f.nayebi@gmail.com>
2018-05-16 12:23:48 -0400

    Documentation for AppProtocol

--- Sources/ApplicationConfiguration/AppProtocol.swift
+++ Sources/ApplicationConfiguration/AppProtocol.swift
@@ -3,6 +3,13 @@ import Foundation
 import PerfectHTTPServer
 import PerfectHTTP
 
+/// A protocol to define Applications
+/// name: application name
+/// config: application configuration
+/// routes: a group of routes
+/// filters: application filters (request & response)
+/// init: to initialize the Application
+/// server to return the server object
 public protocol AppProtocol {
     var name: String { get }
     var config: Configuration? { get}
