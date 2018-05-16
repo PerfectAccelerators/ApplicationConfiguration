0870f58db142e52f21920ef938ab2955113fa79f
conqueror <f.nayebi@gmail.com>
2018-05-16 12:20:04 -0400

    Documentation for AppFilters

--- Sources/ApplicationConfiguration/AppFilters.swift
+++ Sources/ApplicationConfiguration/AppFilters.swift
@@ -2,7 +2,11 @@
 import Foundation
 import PerfectHTTP
 
+/// This struct represents request and response application filters
+/// You can create AppFilters and use it in Application initialization
 public struct AppFilters {
+    /// request filters
     var request: [(HTTPRequestFilter, HTTPFilterPriority)]?
+    /// response filters
     var response: [(HTTPResponseFilter, HTTPFilterPriority)]?
 }
